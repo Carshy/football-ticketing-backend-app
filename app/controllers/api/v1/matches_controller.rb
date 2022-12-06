ccfde672f4f724c0a884946b08e458ed9be5d9de
SimbaWANGU <simbawangulu@gmail.com>
2022-12-06 15:18:47 +0300

    fix linter errors

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -6,15 +6,15 @@ class Api::V1::MatchesController < ApplicationController
 
   def create
     @match = Match.new(match_params)
-      if @match.save
-        render json: @match, status: :created
-      else
-         render json: @match.errors, status: :unprocessable_entity
-      end
+    if @match.save
+      render json: @match, status: :created
+    else
+      render json: @match.errors, status: :unprocessable_entity
+    end
   end
 
   private
-  
+
   def match_params
     params.require(:match).permit(:stadium, :location, :home_team, :away_team, :price, :date, :photo, :user_id)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Simbzz")
+
 Match.create(stadium: 'Stamford Bridge', user_id: 1, location: 'London', price: 40, home_team: 'Chelsea', away_team: 'Arsenal', photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.football.london%2Fchelsea-fc%2Fchelsea-arsenal-live-stream-updates-17606316&psig=AOvVaw3rLgaTrcdt86Y-MEOWnbAG&ust=1670408251005000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNC7yubh5PsCFQAAAAAdAAAAABAO', date: '23/12/2022')
 # Match.create(stadium: 'Emirates', location: 'London', price: 40, home_team: 'Arsenal', away_team: 'Arsenal', photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.football.london%2Fchelsea-fc%2Fchelsea-arsenal-live-stream-updates-17606316&psig=AOvVaw3rLgaTrcdt86Y-MEOWnbAG&ust=1670408251005000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNC7yubh5PsCFQAAAAAdAAAAABAO', date: '26/12/2022')
 # Match.create(stadium: 'Etihad', location: 'Manchester', price: 40, home_team: 'Man City', away_team: 'Arsenal', photo: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.football.london%2Fchelsea-fc%2Fchelsea-arsenal-live-stream-updates-17606316&psig=AOvVaw3rLgaTrcdt86Y-MEOWnbAG&ust=1670408251005000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNC7yubh5PsCFQAAAAAdAAAAABAO', date: '29/12/2022')
