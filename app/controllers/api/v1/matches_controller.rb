e6cd506b5a8a31844c35de71b0de2ea35727a019
SimbaWANGU <simbawangulu@gmail.com>
2022-12-06 15:00:27 +0300

    add create method for matches

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::MatchesController < ApplicationController
     @matches = Match.all
     render json: @matches
   end
+
+  def create
+    @match = Match.new(match_params)
+      if @match.save
+        render json: @match, status: :created
+      else
+         render json: @match.errors, status: :unprocessable_entity
+      end
+  end
+
+  private
+  
+  def match_params
+    params.require(:match).permit(:stadium, :location, :home_team, :away_team, :price, :date, :photo, :user_id)
+  end
 end
