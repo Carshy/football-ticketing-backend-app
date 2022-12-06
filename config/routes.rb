dd050d0172e8c4e04880efba05fd7e04516d8382
SimbaWANGU <simbawangulu@gmail.com>
2022-12-06 15:00:08 +0300

    add create route for matches

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :matches, only: [:index]
+      resources :matches, only: [:index, :create]
     end
   end
   
