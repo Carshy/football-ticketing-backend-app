7db4259dd97befbaa2a3788b034287a95825a61f
ifzyy <johnsonnifemi8@gmail.com>
2022-12-07 11:13:51 +0100

    fixed linters error

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class ApiControllerTest < ActionDispatch::IntegrationTest
-  test "should get v3" do
+  test 'should get v3' do
     get api_v3_url
     assert_response :success
   end
 
-  test "should get tickets" do
+  test 'should get tickets' do
     get api_tickets_url
     assert_response :success
   end
