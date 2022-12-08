b63401fa01dc33d8a8106b9099f101b1d250e033
Collins <mussocollins@gmail.com>
2022-12-08 14:11:59 +0300

    Run migration for quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_103858) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_110942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_103858) do
     t.datetime "updated_at", null: false
     t.string "date"
     t.string "city"
+    t.integer "quantity"
     t.index ["match_id"], name: "index_tickets_on_match_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
