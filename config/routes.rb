3925c08f939a4fbbddfa7381a83cb86c6d262178
Michael <madetunji1376@gmail.com>
2022-12-15 19:19:46 +0100

    errors fixed in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  get "api/v3"
-  get "api/tickets"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
