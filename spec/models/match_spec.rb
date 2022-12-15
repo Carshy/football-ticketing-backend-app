d67ef3ede216e349f52d7a9bcbb5669cbaa38fe2
Michael <madetunji1376@gmail.com>
2022-12-15 21:12:29 +0100

    test linters resolved

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
- user = User.create(name: 'Name')      
- match = Match.new(stadium: 'Stadium', location: 'Location', home_team: 'Home team', away_team: 'Away team', price: 100, date: '2020-10-10', photo: 'app_screenshot.png', user_id: 1)
+  @user = User.create(name: 'Name')
+  match = Match.new(stadium: 'Stadium', location: 'Location', home_team: 'Home team', away_team: 'Away team',
+                    price: 100, date: '2020-10-10', photo: 'app_screenshot.png', user_id: 1)
 
   describe 'checking the match model' do
     it 'should give the stadium name' do
-     match.stadium = 'Stadium'
+      match.stadium = 'Stadium'
       expect(match).to be_valid
     end
 
@@ -16,8 +17,8 @@ RSpec.describe Match, type: :model do
     end
 
     it 'should give away team' do
-        match.away_team = 'Away team'
-        expect(match).to be_valid
+      match.away_team = 'Away team'
+      expect(match).to be_valid
     end
 
     it 'should give the location' do
@@ -26,13 +27,13 @@ RSpec.describe Match, type: :model do
     end
 
     it 'should give the date' do
-        match.date = '2020-10-10'
-        expect(match).to be_valid
+      match.date = '2020-10-10'
+      expect(match).to be_valid
     end
 
     it 'should give the price' do
-        match.price = 100
-        expect(match).to be_valid
+      match.price = 100
+      expect(match).to be_valid
     end
   end
 end
